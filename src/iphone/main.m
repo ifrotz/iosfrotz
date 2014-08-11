baba6101ae2fc0c78a9c238790a385e068a592bf
spathiwa <spathiwa@1854a4dc-a137-0410-8429-7db05d171fc8>
2014-08-11 01:38:46 +0000

    Fix locale support broken when Apple removed POSIX locale support files in IOS 6 by adding back the files to the apps resources and setting an environment var to point to them. Issue 217, and dups.

--- src/iphone/main.m
+++ src/iphone/main.m
@@ -6,11 +6,14 @@ int iphone_main(int argc, char **argv)
 {
     NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-    NSString *localeStr = [[NSLocale currentLocale] localeIdentifier];
-    setlocale(LC_CTYPE, [localeStr UTF8String]); // for wchar routines use by glk libs
+    NSString* resources = [[[NSBundle mainBundle] resourcePath] stringByAppendingString:@"/locale"];
+    static char path_locale[1024];
+    strcpy(path_locale, [resources cStringUsingEncoding:NSASCIIStringEncoding]);
+    setenv("PATH_LOCALE", path_locale, 1);
+    setlocale(LC_CTYPE, "en_US.UTF-8");
+    [pool drain];
 
     UIApplicationMain(argc, argv, nil, @"FrotzAppDelegate");
-
     [pool release];
     fflush(stdout);
     fflush(stderr);
