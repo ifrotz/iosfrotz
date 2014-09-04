7b79cca681d2856cf4bfed41bc2a0571f88480f1
spathiwa <spathiwa@1854a4dc-a137-0410-8429-7db05d171fc8>
2014-09-04 22:41:11 +0000

    Make gestalt correctly return that transparent images are supported, because they are. (For Kerkeruip, other games). Change glk style_Alert to render in bold but not reverse, as that seems to be what other imps do. Turn off noisy warnings for glk sound calls made by games which don’t check gestalt first.  Just silently ignore. Preliminary support for autoscaling glk windows.

--- glkios/ipschan.c
+++ glkios/ipschan.c
@@ -33,36 +33,36 @@ schanid_t glk_schannel_iterate(schanid_t chan, glui32 *rockptr)
 
 glui32 glk_schannel_get_rock(schanid_t chan)
 {
-  gli_strict_warning(L"schannel_get_rock: invalid id.");
+  //gli_strict_warning(L"schannel_get_rock: invalid id.");
   return 0;
 }
 
 glui32 glk_schannel_play(schanid_t chan, glui32 snd)
 {
-  gli_strict_warning(L"schannel_play: invalid id.");
+  //gli_strict_warning(L"schannel_play: invalid id.");
   return 0;
 }
 
 glui32 glk_schannel_play_ext(schanid_t chan, glui32 snd, glui32 repeats,
     glui32 notify)
 {
-  gli_strict_warning(L"schannel_play_ext: invalid id.");
+  //gli_strict_warning(L"schannel_play_ext: invalid id.");
   return 0;
 }
 
 void glk_schannel_stop(schanid_t chan)
 {
-  gli_strict_warning(L"schannel_stop: invalid id.");
+  //gli_strict_warning(L"schannel_stop: invalid id.");
 }
 
 void glk_schannel_set_volume(schanid_t chan, glui32 vol)
 {
-  gli_strict_warning(L"schannel_set_volume: invalid id.");
+  //gli_strict_warning(L"schannel_set_volume: invalid id.");
 }
 
 void glk_sound_load_hint(glui32 snd, glui32 flag)
 {
-  gli_strict_warning(L"schannel_sound_load_hint: invalid id.");
+  //gli_strict_warning(L"schannel_sound_load_hint: invalid id.");
 }
 
 #ifdef GLK_MODULE_SOUND2
