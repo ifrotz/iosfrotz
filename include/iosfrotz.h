b06a4259fec66d109109fc57a300d79a10de3d74
Craig Smith <spathiwa@gmail.com>
2017-09-25 18:08:37 -0500

    Update release notes.

--- include/iosfrotz.h
+++ include/iosfrotz.h
@@ -151,8 +151,8 @@ extern bool gUseSplitVC;
 
 #define UseFullSizeStatusLineFont 1
 
-#define IPHONE_FROTZ_RN_VERS "1.8"
 #define IPHONE_FROTZ_VERS "1.8.1"
+#define IPHONE_FROTZ_RN_VERS IPHONE_FROTZ_VERS // "1.8"
 #define FROTZ_BETA 0
 
 #define APPLE_FASCISM 0 // (!FROTZ_BETA)
